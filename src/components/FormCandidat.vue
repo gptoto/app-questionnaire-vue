d7b9b5c71840b33a1d7740bcf202b957a5f2d462
Hamon <hamonaxel@live.fr>
2019-10-15 16:13:20 +0200

    PROJET FONCTIONNEL

---
+++ src/components/FormCandidat.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="formulaireCandidat">
+    <h1>coucou</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FormCandidat'
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <FormCandidat/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import FormCandidat from '@/components/FormCandidat.vue'
 
 export default {
   name: 'home',
   components: {
-    HelloWorld
+    FormCandidat
   }
 }
 </script>
